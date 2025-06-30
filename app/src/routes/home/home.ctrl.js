cdc792e449dd7d24c0fc06f0c95efa18db3c6cfe
LDH <leedoh0384@gmail.com>
2025-06-30 15:26:44 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,29 @@ const output = {
     },
 };
 
+const users = {
+    id: ["123", "이동훈", "dlehdgns"],
+    psword: ["123", "1234", "321"],
+};
+
 const process = {
     login: (req, res ) => {
-        console.log(req.body);
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx]=== psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다.",
+        });
     },
 };
 
